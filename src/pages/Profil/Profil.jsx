d2173a0ffbecc78a6f9a733457dde4ca90486745
Foxy WhiteTrack | One Trick <48949675+Foxy-WhiteTrack@users.noreply.github.com>
2023-09-17 00:08:37 +0200

    Le score est implémenté dans un composant

--- src/pages/Profil/Profil.jsx
+++ src/pages/Profil/Profil.jsx
@@ -3,20 +3,14 @@ import { useParams } from 'react-router-dom';
 import './Profil.css';
 import { getUserDataById } from '../../api/callApi.js';
 
-import { RadialBarChart, RadialBar, Legend } from 'recharts';
+import Goals from '../../components/Goals/Goals';
 
 
 export default function Profil() {
     const { id } = useParams();
     const [userData, setUserData] = useState({});
 
-    const radialData = [
-        { name: 'Score', value: userData.todayScore ? userData.todayScore * 100 : 0 },
-    ];
 
-    const scorePercentage = userData.todayScore ? userData.todayScore * 100 : 0;
-    const startAngle = 0;
-    const endAngle = (scorePercentage / 100) * 360;
 
 
 
@@ -50,33 +44,7 @@ export default function Profil() {
 
                 </div>
                 <div className='score'>
-                    <div className='title-score'>
-                        <p className=''>Score</p>
-                    </div>
-                    <div className='donut-score'>
-                        <RadialBarChart
-                            width={200}
-                            height={200}
-                            cx={100}
-                            cy={100}
-                            innerRadius={40}
-                            outerRadius={80}
-                            barSize={8}
-                            data={radialData}
-                            label={() => ''}
-                            startAngle={startAngle}
-                            endAngle={endAngle}
-                        >
-                            <RadialBar minAngle={15} background dataKey='value' fill='#FF0000' />
-                            <Legend iconSize={0} />
-                        </RadialBarChart>
-
-                    </div>
-
-                    <div className='results-score'>
-                        <p className='data-score'>{userData.todayScore ? userData.todayScore * 100 : ''} %</p>
-                        <p className='text-score'>de votre objectif</p>
-                    </div>
+                    <Goals userId={id} />
                 </div>
             </div>
 
