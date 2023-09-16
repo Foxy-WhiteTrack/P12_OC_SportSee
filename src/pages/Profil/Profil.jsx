a1b60249ebe72278791ec4cc07cd993834654c18
Foxy WhiteTrack | One Trick <48949675+Foxy-WhiteTrack@users.noreply.github.com>
2023-09-16 18:39:55 +0200

    Ajout de l'import du profil de l'utilisateur sur la page profil

--- src/pages/Profil/Profil.jsx
+++ src/pages/Profil/Profil.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import './Profil.css';
+import { getUserDataById } from '../../api/callApi.js';
 
 export default function Profil() {
-
+    const { id } = useParams();
     const [userData, setUserData] = useState({});
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+
+                const user = await getUserDataById(id);
+                setUserData(user);
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données :', error);
+            }
+        }
+
+        fetchData();
+    }, [id]);
 
     return (
         <>
             <div className='profil'>
-                <h1 className='profile__hello'>Bonjour <span>User</span></h1>
-                <p className='profile__congrats'>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+                <h1 className='profile__hello'>Bonjour <span>{userData.userInfos ? userData.userInfos.firstName : ''}</span></h1>
+                <p className='profile__congrats'>Félicitations ! Vous avez explosé vos objectifs hier 👏</p>
             </div>
         </>
     );
